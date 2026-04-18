b8d1a16a6f5b1f23fc712f19b1dd8a6554373a4e
George Graciosa <george.graciosa@gmail.com>
2026-04-18 15:41:29 -0300

    primeiro commit

---
+++ atividade1/index.js
@@ -0,0 +1,8 @@
+// Escreva uma função em qualquer linguagem (se possível C#) que:
+//    Recebe uma lista de números.
+//    Retorna o segundo maior número distinto.
+//    Se não houver, retorne null.
+// Exemplo:
+// Input: [3, 2, 1, 5, 5, 4] → Output: 4 (pois o maior é 5, e o segundo maior é 4).
+
+// * Não é necessário frontend. Retorno pode ser via terminal.
