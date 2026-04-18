11e22e55cfc13845e82ac835513e76741b1d2d0d
George Graciosa <george.graciosa@gmail.com>
2026-04-18 16:53:54 -0300

    primeiro commit atividade 2

---
+++ atividade2/index.js
@@ -0,0 +1,27 @@
+// Escreva um programa em qualquer linguagem que receberá como input uma lista de alunos e uma lista de notas. O programa deverá:
+// a) exibir o nome e a situação de cada aluno:
+//    * nota >= 7: Aprovado
+//    * nota >= 5 e < 7: Recuperação
+//    * nota < 5: Reprovado
+// b) mostrar a quantidade de aprovados,  quantidade de recuperação e  quantidade de reprovados.
+
+// Exemplo:
+// Entrada:
+// nomes = ["Ana", "Bruno", "Carlos", "Diana"]
+// notas = [8, 4, 7, 6]
+
+// Saída:
+// Ana -> Aprovada
+// Bruno -> Reprovado
+// Carlos -> Aprovado
+// Diana -> Recuperação
+
+// Aprovados: 2
+// Recuperação: 1
+// Reprovados: 1
+
+// * O nome do método é de sua escolha
+// * O formato/tipagem dos inputs é de sua escolha
+// * O formato/estilização da saída é de sua escolha
+// * As validações são de sua escolha.
+// * Não é necessário frontend. Retorno pode ser via terminal.
