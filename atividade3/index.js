911cb66f64f077b66a0e67adb4de2885831ef15a
George Graciosa <george.graciosa@gmail.com>
2026-04-20 12:27:47 -0300

    primeiro commit atividade 3

---
+++ atividade3/index.js
@@ -0,0 +1,6 @@
+// Crie uma aplicação com as seguintes funcionalidades:
+//    * Cadastrar uma tarefa (com título, descrição, data de vencimento e status)
+//    * Alterar o status da tarefa (Pendente, Em Andamento, Concluída)
+//    * Listar tarefas ordenadas (decrescente) por data de vencimento
+
+// * Pode ser somente frontend.
